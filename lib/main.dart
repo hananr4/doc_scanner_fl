a2f03e3d76d478f221e8c259d20adeb6d0a5f46e
Hanan Rodriguez <hanan.rodriguez@greensoft.com.ec>
2022-01-27 20:28:38 -0500

    instalar ejemplo

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,101 @@
+import 'dart:io';
+
+import 'package:document_scanner_flutter/document_scanner_flutter.dart';
+import 'package:document_scanner_flutter/configs/configs.dart';
 import 'package:flutter/material.dart';
+import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PDFDocument? _scannedDocument;
+  File? _scannedDocumentFile;
+  File? _scannedImage;
+
+  openPdfScanner(BuildContext context) async {
+    var doc = await DocumentScannerFlutter.launchForPdf(
+      context,
+      labelsConfig: {
+        ScannerLabelsConfig.ANDROID_NEXT_BUTTON_LABEL: "Next Steps",
+        ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_SINGLE: "Only 1 Page",
+        ScannerLabelsConfig.PDF_GALLERY_FILLED_TITLE_MULTIPLE:
+            "Only {PAGES_COUNT} Page"
+      },
+      //source: ScannerFileSource.CAMERA
+    );
+    if (doc != null) {
+      _scannedDocument = null;
+      setState(() {});
+      await Future.delayed(const Duration(milliseconds: 100));
+      _scannedDocumentFile = doc;
+      _scannedDocument = await PDFDocument.fromFile(doc);
+      setState(() {});
+    }
+  }
+
+  openImageScanner(BuildContext context) async {
+    var image = await DocumentScannerFlutter.launch(context,
+        //source: ScannerFileSource.CAMERA,
+        labelsConfig: {
+          ScannerLabelsConfig.ANDROID_NEXT_BUTTON_LABEL: "Next Step",
+          ScannerLabelsConfig.ANDROID_OK_LABEL: "OK"
+        });
+    if (image != null) {
+      _scannedImage = image;
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text('Document Scanner Demo'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (_scannedDocument != null || _scannedImage != null) ...[
+              if (_scannedImage != null)
+                Image.file(_scannedImage!,
+                    width: 300, height: 300, fit: BoxFit.contain),
+              if (_scannedDocument != null)
+                Expanded(
+                    child: PDFViewer(
+                  document: _scannedDocument!,
+                )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                    _scannedDocumentFile?.path ?? _scannedImage?.path ?? ''),
+              ),
+            ],
+            Center(
+              child: Builder(builder: (context) {
+                return ElevatedButton(
+                    onPressed: () => openPdfScanner(context),
+                    child: const Text("PDF Scan"));
+              }),
+            ),
+            Center(
+              child: Builder(builder: (context) {
+                return ElevatedButton(
+                    onPressed: () => openImageScanner(context),
+                    child: const Text("Image Scan"));
+              }),
+            )
+          ],
         ),
       ),
     );
